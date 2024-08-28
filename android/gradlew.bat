5ca0d3ddfb31a193f8c621865a95fd3717774425
Bhavesh Yadav <56354230+bhavesh-10@users.noreply.github.com>
2024-08-28 12:18:52 +0530

    Update gradlew.bat

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -8,10 +8,10 @@
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
+@rem distributed under the License is distributed on an "AS IS" BASIS
+@rem 
+@rem See
+@rem 
 @rem
 
 @if "%DEBUG%"=="" @echo off
